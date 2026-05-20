4b1e8beab003296c6258ce79f5709e576be9509f
Hudhaifa-22 <114759504+Hudhaifa-22@users.noreply.github.com>
2026-05-20 19:14:52 +0530

    Add files via upload

--- inventproj/inventory/urls.py
+++ inventproj/inventory/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import CustomLoginView
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('add/', views.product_create, name='product_create'),
     path('edit/<int:pk>/', views.product_update, name='product_update'),
     path('delete/<int:pk>/', views.product_delete, name='product_delete'),
-]
+    path('register/', views.register_view, name='register'),
+
+    path('login/', CustomLoginView.as_view(), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    ]
--- inventproj/inventory/views.py
+++ inventproj/inventory/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect, get_object_or_404
+from urllib3 import request
 from .models import Product
 from .forms import ProductForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-
+@login_required(login_url='login')
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'inventory/prod_list.html', {'products': products})
 
-
+@login_required(login_url='login') 
 def product_create(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
@@ -22,6 +26,7 @@ def product_create(request):
     return render(request, 'inventory/prod_form.html', {'form': form})
 
 
+@login_required(login_url='login')
 def product_update(request, pk):
     product = get_object_or_404(Product, pk=pk)
 
@@ -36,6 +41,7 @@ def product_update(request, pk):
     return render(request, 'inventory/prod_form.html', {'form': form})
 
 
+@login_required(login_url='login')
 def product_delete(request, pk):
     product = get_object_or_404(Product, pk=pk)
 
@@ -44,3 +50,29 @@ def product_delete(request, pk):
         return redirect('product_list')
 
     return render(request, 'inventory/product_delete.html', {'product': product})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('product_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+
+    template_name = 'registration/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+
+
+        if 'next' in request.GET:
+            messages.warning(
+                request,
+                "Please login to access inventory pages."
+            )
+
+        return super().dispatch(request, *args, **kwargs)
